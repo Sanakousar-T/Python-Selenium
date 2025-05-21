d60d7a9317dde92ceb31fd5b439f9ff84cb9e038
Sanakousar <60170093+Sanakousar-T@users.noreply.github.com>
2025-05-21 10:07:13 +0530

    1st commit

---
+++ 1.introduction of selenium, History and Flavours.py
@@ -0,0 +1,103 @@
+
+
+"""
+Automation:
+it is a set of instruction/steps to perform automatically is called as automation.
+
+selenium:
+it is a tool/software which is used to perform automation.
+
+python:
+is a programming lang which is used to write set of instruction which need to be do automatically.
+
+there are 3 types of application are present,
+------------------------------------------------------
+1.standalone/desktop application:
+-------------------------------------------
+*any application which we can access without opening browser, without entering url, without internet connection, without server
+then those applications are called as standalone application.
+example: paint, notepad, ms-office , calculator, alram, clock, flashlight, calender, etc..
+------------
+2.client-server/mobile application:
+--------------------------------------------
+*there are 2 application will be present that is client and server application.
+*client application will interact with server application and required information.
+example: whats app, instagram, gamil, facebook, twitter, etc..
+------------
+3.web based application:
+-------------------------------
+*any application which we can access by opening the browser, entering url, with internet, withserver connection then those application
+are called as web-based application.
+example:
+-----------
+www.fb.com, www.student.qspiders.com, www.instgram.com, etc..
+"""
+
+"""
+what is selenium?
+----------------------
+*selenium is a free and open source web-based autmation tool.
+(free--> anyone can download and use from google,
+open source--> the source code of selenium is freely available in google)
+*selenium will support multiple programming lang.
+(python, java, c#/.net, java script, r, go, etc..)
+*selenium will support multiple browsers.
+(chrome, edge, firefox, safari, etc.)
+*selenium will support multiple OS.
+(windows,mac, linux)
+*the official websit of selenium is
+https://www.selenium.dev/
+
+dis-advantages of selenium:
+------------------------------------
+*we can't automate client-server and stand alone application.
+*we can't automate OTP, captch, image, animation, sub-title, audio, video, i'm not a robot, etc..
+
+history of selenium
+-------------------------
+*selenium was developed by Jason Huggins in a year 2004 in a thought works company as a internal tool.
+*later he joined to Google in a year 2007, then Google acquired selenium and the current owner is google.
+====================================================================================================
+
+falvours/types of selenium:
+-----------------------------------
+*there are 4 types of selenium is present,
+1.selenium IDE(integerated development environement)
+2.selenium RC(remote control)
+3.selenium webdriver
+4.selenium grid
+
+1.selenium IDE:
+--------------------
+*it  will do simple record-and-playback of interactions with the browser.
+
+2.selenium webdriver:
+-----------------------------
+*a updated version of selenium RC is selenium webdriver
+*the latest version of selenium is selenium4.
+
+selenium webdriver architecture diagram
+-----------------------------------------------------
+
+explanation:
+---------------
+*a Test Engineer will write set of instruction/script by using a programming lang is called as client/lang binding.
+*a set of instruction will perform automatically on  browser by using selenium web-driver.
+*a selenium webdriver will follow W3C protocol(World Wide Web Consortium).
+
+3.selenium grid
+--------------------
+*the tests against a vast combination of browsers/OS, then you want to use Selenium Grid
+
+========================================================================================================
+selenium installation:
+---------------------------
+*python should be installed(latest version)
+*install pycharm(latest version)
+*open pycharm and create a project.
+*install selenium
+(click on 4lines on left top corner --> click on file --> click on settings --> click on project --> click on python interpreter -->
+click on + --> search for selenium --> click on install package --> a successfull messgae should be display)
+*create a directory called program in pycharm project.
+*create a new python file in program directory.
+"""
