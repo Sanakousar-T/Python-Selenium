3c063ea41b2d90b67985491d06d0e9bf9d61d5a6
Sanakousar <60170093+Sanakousar-T@users.noreply.github.com>
2025-05-21 13:23:45 +0530

    html

---
+++ 6. HTML.py
@@ -0,0 +1,44 @@
+"""
+html:
+-----
+* html stands for hyper text markup language.
+* html is used to design and develop the webpages.
+* a webpage will be developed by html tags.
+* a keyword which is enclosed within angular brackets(<keyword>) is called as tags.
+* if we open any tag it is mandatory to close the tag(</keyword>).
+
+general structure of html:
+==========================
+<html>
+    <head>
+        <title> title of the webpage </title>
+    </head>
+    <body>
+        #all the components
+    </body>
+</html>
+
+steps to create a webpage:
+==========================
+step1: open notepad
+step2: type the html code and save with .html extension
+step3: select save as type and "all files" option
+"""
+"""
+<html>
+  <head> 
+	<title>DemowebpageM7batch</title>
+  </head>
+  <body bgcolor="pink">
+    Username:<input type="text" id="a1"><br/>
+	Password:<input type="password" id="a2"><br/>
+	<input type="radio" name="n1">New user
+   	<input type="radio" name="n2">Old user<br/>
+	<input type="checkbox" value="v1">I Accept T&C<br/>
+	<a href="http://www.google.com" class="c1">ForgottenPassword</a><br/>
+	<img src="C:\\Users\\Hp\\Downloads\\selenium.jpg" width=100><br/>
+	<input type="submit" class="c2">
+	<input type="reset" value="cancel">
+  </body>
+</html>
+"""
